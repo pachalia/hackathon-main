69848f2ccda75aa48b4016965841f3bfac057077
Andrey <pachalia2009@yandex.com>
2024-11-30 20:13:21 +0300

    Поправил код и добавил модуль message

--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,37 @@
 import {Menu} from './core/menu'
 import {BackgroundModule} from "@/modules/background.module";
 import {ShapeModule} from "@/modules/shape.module";
+import {MessageModule} from "@/modules/message.module";
 
 export class ContextMenu extends Menu {
 
+    modulesArray = [
+        new BackgroundModule('backround', 'Поменять цвет'),
+        new ShapeModule('shape','фигура'),
+        new MessageModule('message', 'Вызвать сообщение')
+    ]
+    menu = document.querySelector('.menu')
+    body = document.body
+
+
     constructor() {
         super();
-        this.menu = document.querySelector('.menu')
-        this.modulesArray = [new BackgroundModule('backround', 'фон'), new ShapeModule('shape','фигура')]
+        this.body.addEventListener('contextmenu', (e) =>{
+            e.preventDefault()
+            this.open(`${e.pageX}px`, `${e.pageY}px`)
+        })
+        this.body.addEventListener('click', () => {
+            this.close()
+        })
     }
+
 
     open(x,y) {
         this.menu.style.display = 'block'
         this.menu.style.top = y
         this.menu.style.left = x
         const arr =  this.modulesArray.map((module) =>module.toHTML())
-        this.menu.innerHTML= arr
+        this.menu.innerHTML= arr.join('')
         const menuItem = document.querySelectorAll('.menu-item')
         menuItem.forEach((val) =>val.addEventListener('click', (e) => {
             const type = e.target.dataset.type
---
+++ src/modules/message.module.js
@@ -0,0 +1,37 @@
+import {Module} from "@/core/module";
+import {random} from '@/utils'
+
+export class MessageModule extends Module {
+
+    words = [
+        'Солнце, небо, птицы', 'Дождь, лужи, отражение', 'Цветы, луг, аромат', 'Луна, звезды, ночь',
+        'Лес, природа, тишина', 'Море, волны, песок', 'Ветер, листва, шепот'
+    ]
+    body = document.body
+    constructor(type,text) {
+        super(type,text);
+    }
+
+    trigger() {
+       const message =document.querySelector('.message')
+       message ? message.remove() : null
+       const el = document.createElement('div')
+       el.innerText = this.randomText()
+       el.className = 'message'
+       el.style.background = 'blue'
+       el.style.color = 'red'
+       el.style.fontSize = '20px'
+       el.style.position = 'absolute'
+       el.style.top = '90%'
+       el.style.right = '2%'
+       el.style.padding = '4px'
+        setTimeout(()=>{
+            document.querySelector('.message').remove()
+       }, 5000)
+       this.body.append(el)
+    }
+
+    randomText() {
+        return this.words[random(0, this.words.length-1)]
+    }
+}
