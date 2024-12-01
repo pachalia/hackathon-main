74ab5816d3b9382ba12487bf65512e67b455aecf
ShtrihKot <sasha.prohorenko.97@mail.ru>
2024-12-01 11:58:49 +0400

    add other shapes

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -12,15 +12,15 @@ export class ShapeModule extends Module {
         /// #создаю канвас
         const myCanvas = document.createElement("canvas");
         myCanvas.id = this.shapesList.length + 1;//надо ли?
-        myCanvas.width = 200;
-        myCanvas.height = 200;
+        myCanvas.width = 150;
+        myCanvas.height = 150;
         myCanvas.style.position = 'absolute';
         myCanvas.style.left = `${random(1, window.innerWidth)}px`;
         myCanvas.style.top = `${random(1, window.innerHeight)}px`;
         document.body.append(myCanvas);
         const ctx = myCanvas.getContext("2d");
 
-        this.renderStar(ctx);//this.renderRandomShape(ctx);
+        this.renderSquare(ctx);//this.renderRandomShape(ctx);
 
 
     }
@@ -29,36 +29,59 @@ export class ShapeModule extends Module {
         randScale = random(20, 150),
         randColor = 'blue',
         randRotate = random(0, 360)) {
-
-        canvas.scale(randScale/ 150, randScale / 150);
-        canvas.translate(70,70);
-        canvas.rotate(randRotate * Math.PI / 180);
-        canvas.beginPath();
-        canvas.moveTo(0 - 60, 40 - 60);
-        canvas.lineTo(120 - 60, 40 - 60);
-        canvas.lineTo(20 - 60, 120 - 60);
-        canvas.lineTo(60 - 60, 0 - 60);
-        canvas.lineTo(100 - 60, 120 - 60);
-        canvas.lineTo(0 - 60, 40 - 60);
-        canvas.strokeStyle = randColor;
-        canvas.stroke();
-        canvas.fillStyle = randColor;
-        canvas.fill();
+            canvas.scale(randScale/ 150, randScale / 150);
+            canvas.translate(70,70);
+            canvas.rotate(randRotate * Math.PI / 180);
+            canvas.beginPath();
+            canvas.moveTo(0 - 60, 40 - 60);
+            canvas.lineTo(120 - 60, 40 - 60);
+            canvas.lineTo(20 - 60, 120 - 60);
+            canvas.lineTo(60 - 60, 0 - 60);
+            canvas.lineTo(100 - 60, 120 - 60);
+            canvas.lineTo(0 - 60, 40 - 60);
+            canvas.strokeStyle = randColor;
+            canvas.stroke();
+            canvas.fillStyle = randColor;
+            canvas.fill();
     }
-    renderCircle() {
-
+    renderCircle(canvas,
+        randScale = random(20, 150),
+        randColor = 'blue') {
+            canvas.scale(randScale/ 150, randScale / 150);
+            canvas.translate(75,75);
+            canvas.beginPath();
+            canvas.arc(0, 0, 70 , 0, 2 * Math.PI);
+            canvas.strokeStyle = randColor;
+            canvas.stroke();
+            canvas.fillStyle = randColor;
+            canvas.fill();
     }
-    renderSquare() {
-
+    renderSquare(canvas,
+        randScale = random(20, 150),
+        randColor = 'blue',
+        randRotate = random(0, 360)) {
+            canvas.scale(randScale/ 150, randScale / 150);
+            canvas.translate(75, 75);
+            canvas.rotate(randRotate * Math.PI / 180);
+            canvas.fillStyle = randColor;
+            canvas.fillRect(-53, -53, 106, 106);
     }
-    renderTriangle() {
-
+    renderTriangle(canvas,
+        randScale = random(20, 150),
+        randColor = 'blue',
+        randRotate = random(0, 360)) {
+            canvas.scale(randScale/ 150, randScale / 150);
+            canvas.translate(75, 75);
+            canvas.rotate(randRotate * Math.PI / 180);
+            canvas.beginPath();
+            canvas.moveTo(-65, -37);
+            canvas.lineTo(0, 74);
+            canvas.lineTo(65, -37);
+            canvas.lineTo(-65, -37);
+            canvas.strokeStyle = randColor;
+            canvas.stroke();
+            canvas.fillStyle = randColor;
+            canvas.fill();
     }
 
 }
-
-// circle
-// ctx2.beginPath();
-// ctx2.arc(100, 75, 50, 0, 2 * Math.PI);
-// ctx2.translate(0,0)
-// ctx2.stroke();
