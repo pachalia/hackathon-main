385a2a523977875df5548fabf60e77dc3113d10b
Anna Polikarpova <polikarpovaanna2004@gmail.com>
2024-12-01 20:03:43 +0500

    change html to js

--- src/modules/questionnaire.module.js
+++ src/modules/questionnaire.module.js
@@ -7,76 +7,120 @@ export class QuestionnaireModule extends Module {
 
     trigger() {
         const body = document.body
-        body.append(this.getAnswers())
+        body.append(this.createForm())
     }
 
-    getAnswers() {
-        const questionnaire = document.body
-        questionnaire.innerHTML = `  <h1>Расскажите немного о себе</h1>
-
-        <div class="question">
-        <label for="gender">Ваш пол:</label><br>
-        <input type="radio" id="male" name="gender" value="мужской">
-        <label for="male">Мужской</label><br>
-        <input type="radio" id="female" name="gender" value="женский">
-        <label for="female">Женский</label>
-        </div>
-
-        <div class="question">
-        <label for="city">Ваш город:</label><br>
-        <input type="text" id="city" name="city">
-        </div>
-
-        <div class="question">
-        <label for="drink">Ваш любимый напиток:</label><br>
-        <input type="checkbox" id="coffee" name="drink" value="кофе">
-        <label for="coffee">Кофе</label><br>
-        <input type="checkbox" id="tea" name="drink" value="чай">
-        <label for="tea">Чай</label><br>
-        <input type="checkbox" id="juice" name="drink" value="сок">
-        <label for="juice">Сок</label><br>
-        <input type="checkbox" id="water" name="drink" value="вода">
-        <label for="water">Вода</label>
-        </div>
-
-        <div class="question">
-        <label for="vacation">Какой отдых предпочитаете?</label><br>
-        <select id="vacation" name="vacation">
-            <option value="">Выберите вариант</option>
-            <option value="море">На море</option>
-            <option value="горы">В горах</option>
-            <option value="экскурсии">Экскурсии</option>
-            <option value="дома">Дома</option>
-        </select>
-        </div>
-
-        <div class="question">
-        <label for="mood">Оцените свое настроение от 1 до 5:</label><br>
-        <input type="range" id="mood" name="mood" min="1" max="5">
-        </div>
-    
-        <button onclick="submitForm()">Отправить анкету</button>`
-
-        //body.style.cssText = 'background: blue;'
-        questionnaire.style.cssText = 'display: flex; flex-direction: column; width: 40%; justify-contnet: center; align-items: left; background: #DCDCDC;'
-
-        //отправка анкеты
-            const submitForm = () => {
-            //получаем значения всех полей
-            const gender = document.querySelector('input[name="gender"]:checked').value;
-            const city = document.getElementById("city").value;
-            const drink = [];
-            document.querySelectorAll('input[name="drink"]:checked').forEach(item => {
-                drink.push(item.value);
-            });
-            const vacation = document.getElementById("vacation").value;
-            const mood = document.getElementById("mood").value;
-                
-            //выводим результаты в консоль
-            console.log({gender, city, drink, vacation, mood});
-    
-            alert("Спасибо за заполнение анкеты!");
+    createForm = () => {
+        const body = document.body
+          //заголовок
+        const nameQuestionnaire = document.createElement('h2')
+        nameQuestionnaire.textContent = 'Расскажите немного о себе'
+        body.append(nameQuestionnaire)
+        
+        //создание input
+        const createInput = (type, id, name, value) => {
+          const input = document.createElement('input')
+          input.type = type
+          input.id = id
+          input.name = name 
+          input.value = value
+          return input
         }
+        
+        //создание label
+        const createLabel = (text) => {
+          const label = document.createElement('label')
+          label.textContent = text
+          return label
+        }
+        
+        //1
+        const gender = createLabel('Ваш пол')
+        body.append(gender)
+          
+        const genderVar1 = createInput('radio', 'male', 'gender', 'мужской')
+        gender.appendChild(genderVar1);
+        const labelMale = createLabel('Мужской');
+        gender.appendChild(labelMale);
+        
+        const genderVar2 = createInput('radio', 'female', 'gender', 'женский');
+        gender.appendChild(genderVar2);
+        const labelFemale = createLabel('Женский');
+        gender.appendChild(labelFemale);
+        
+        //2
+        const city = document.createElement('div');
+        body.append(city)
+        
+        const labelCity = createLabel('Ваш город: ');
+        body.appendChild(labelCity);
+        
+        const inputCity = createInput('text', 'city', 'city', '');
+        labelCity.appendChild(inputCity);
+        
+        //3
+        const drink = document.createElement('div');
+        body.appendChild(drink);
+        
+        const labelDrink = createLabel('Ваш любимый напиток:');
+        drink.appendChild(labelDrink);
+        
+        const drinkVar1 = createInput('checkbox', 'coffee', 'drink', 'кофе');
+        drink.appendChild(drinkVar1);
+        const labelCoffee = createLabel('Кофе', 'coffee');
+        drink.appendChild(labelCoffee);
+        
+        const drinkVar2 = createInput('checkbox', 'tea', 'drink', 'чай');
+        drink.appendChild(drinkVar2);
+        const labelTea = createLabel('Чай', 'tea');
+        drink.appendChild(labelTea);
+        
+        const drinkVar3 = createInput('checkbox', 'juice', 'drink', 'сок');
+        drink.appendChild(drinkVar3);
+        const labelJuice = createLabel('Сок', 'juice');
+        drink.appendChild(labelJuice);
+        
+        const drinkVar4 = createInput('checkbox', 'water', 'drink', 'вода');
+        drink.appendChild(drinkVar4);
+        const labelWater = createLabel('Вода', 'water');
+        drink.appendChild(labelWater);
+        
+        //4
+        const mood = document.createElement('div');
+        body.appendChild(mood);
+        
+        const labelMood = createLabel('Оцените свое настроение от 1 до 5:');
+        mood.appendChild(labelMood);
+        
+        const moodVar = createInput('range', 'mood', 'mood', '');
+        moodVar.min = '1'
+        moodVar.max = '5'
+        mood.appendChild(moodVar);
+        
+        //кнопка
+        const button = document.createElement("button");
+        button.textContent = "Отправить анкету";
+        
+        body.appendChild(button);
+        
+        //отправка анкеты
+        const submitForm = () => {
+          //получаем значения всех полей
+          const gender = document.querySelector('input[name="gender"]:checked').value;
+          console.log(gender)
+          const city = document.getElementById("city").value;
+          const drink = [];
+          document.querySelectorAll('input[name="drink"]:checked').forEach(item => {
+          drink.push(item.value);
+          });  
+          const mood = document.getElementById("mood").value;
+                    
+          //выводим результаты в консоль
+          console.log({gender, city, drink, mood});
+          
+          alert("Спасибо за заполнение анкеты!");
+        }
+        
+        button.onclick = submitForm;
     }
 }
-
